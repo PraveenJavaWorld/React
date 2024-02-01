4a07d1ef84b5d3dd3e0ee0bbc2c21d21a328353c
Praveen-Chandana <praveen97javaworld@gmail.com>
2024-02-02 00:42:52 +0530

    Implemeted Accordian

--- Food_Ordering_App_TailwindCSS/src/components/RestaurantMenu.js
+++ Food_Ordering_App_TailwindCSS/src/components/RestaurantMenu.js
@@ -1,34 +1,35 @@
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
 
-    const {resId} = useParams();
+    const { resId } = useParams();
 
     const resInfo = useRestaurantMenu(resId);
 
-    if(resInfo === null){
+    if (resInfo === null) {
         return <Shimmer />;
     }
 
-    const {name, cuisines, costForTwoMessage} = resInfo?.cards[0]?.card?.card?.info;
+    const { name, cuisines, costForTwoMessage } = resInfo?.cards[0]?.card?.card?.info;
 
-    const {itemCards} = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+    const { itemCards } = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
 
-  return (
-    <div className="restaurant-menu">
-        <h1>{name}</h1>
-        <h3>{cuisines.join(", ")}</h3>
-        <h3>{ costForTwoMessage}</h3>
-        <h2>Menu</h2>
-        <ul>
+    const categories = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards;
+
+    const categoriesFilteredList = categories.filter(c => c.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
+
+    return (
+        <div className="text-center">
+            <h1 className="font-bold my-6 text-2xl">{name}</h1>
+            <p className="font-bold text-lg">{cuisines.join(", ")} - {costForTwoMessage}</p>
             {
-                itemCards?.map(item => <li key={item?.card?.info?.id}>{item?.card?.info?.name} - Rs.{item?.card?.info?.price/100}/-</li>)
+                categoriesFilteredList.map(category => <RestaurantCategory key={category?.card?.card?.title} data={category?.card?.card} />)
             }
-        </ul>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default RestaurantMenu
